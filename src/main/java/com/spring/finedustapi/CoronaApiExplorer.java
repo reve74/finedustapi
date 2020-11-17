cda8759f9f74abc6f4e0af50202e54c7d401c13a
reve74 <reve99@naver.com>
2020-11-17 20:17:16 +0900

    20202017

---
+++ src/main/java/com/spring/finedustapi/CoronaApiExplorer.java
@@ -0,0 +1,40 @@
+package com.spring.finedustapi;
+
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class CoronaApiExplorer {
+    public static void main(String[] args) throws IOException {
+
+   	
+        StringBuilder urlBuilder = new StringBuilder("http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson"); /*URL*/
+        urlBuilder.append("?ServiceKey=QVWDM9ZUKGDJ%2BZ46qFzKBatWi3Kc8Fp1WdNpIaERUzVjt0Xq4ha9p7BYowPw079DWuvSKjQrXlIFSSI8%2FhvE0w%3D%3D"); /*Service Key*/
+        urlBuilder.append("&pageNo=1"); /*페이지번호*/
+        urlBuilder.append("&numOfRows=10"); /*한 페이지 결과 수*/
+        urlBuilder.append("&startCreateDt=20201117"); /*검색할 생성일 범위의 시작*/
+        urlBuilder.append("&endCreateDt=20201117"); /*검색할 생성일 범위의 종료*/
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        System.out.println("Response code: " + conn.getResponseCode());
+        BufferedReader rd;
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+        System.out.println(sb.toString());
+    }
+}
